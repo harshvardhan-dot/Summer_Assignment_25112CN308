199459bfc12093581ef12ffcc33685ae5a8a621c
harshvardhan-dot <vardhanharsh012345@gmail.com>
2026-06-05 00:10:28 +0530

    added day5

---
+++ day5/Q17_perfectnumber.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isperfect(int n){
+    int sum  =0 ;
+    for (int i = 1; i <= n/2; i++)
+    {
+        if (n%i == 0)
+        {
+            sum += i;
+        }
+        
+    }
+   return sum == n; 
+    
+}
+int main() {
+    int num;
+
+    cout << "enter the number"<< endl;
+    cin >> num;
+    if(isperfect(num))
+        cout << "the number is perfect"<<endl;
+    
+    else
+        cout<< "the number is not perfect"<<endl;
+    
+
+
+    return 0;
+}
---
+++ day5/Q18_strongnumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact =1 ; 
+    if (n == 0){
+        fact = 1;
+    }
+    else{
+    for (int i = 0; i < n; i++)
+    {
+        fact *= i;
+        
+    }
+}
+return fact ;
+
+}
+int main() {
+    int num ,digit,sum =0 ;
+    cout << "enter the number " << endl;
+    cin >> num ;
+    while ( num != 0)
+    {
+        digit = num%10;
+        sum += factorial(digit);
+        num =num/10;
+    }
+    if(sum == num){
+        cout << "the number is strong number";
+
+    }
+    
+    return 0;
+}
