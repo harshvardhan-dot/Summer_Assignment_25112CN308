7d847fb33d9180f1221b641cc4a404e9225608be
harshvardhan-dot <vardhanharsh012345@gmail.com>
2026-06-07 23:46:28 +0530

    corrected day8 code

--- day9/Q35_repeatedcharacterpattern.cpp
+++ day9/Q35_repeatedcharacterpattern.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 void pattern(int n){
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
       char   ch ='A' + i;
         for (int j =0  ; j <=i; j ++ )
--- day9/Q36_hollowsquare.cpp
+++ day9/Q36_hollowsquare.cpp
@@ -3,11 +3,18 @@ using namespace std;
 void pattern(int n){
     for (int i = 0; i < n; i++)
     {
-        for (int j = n -i ; j >=1; j--)
+        for (int j = 0 ; j < n; j++)
         {
-            cout<< "*";
+            if (i==0||j==0||j==n-1||i==n-1)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+            
         }
-        cout<< endl;
+        cout<<endl;
         
     }
     
