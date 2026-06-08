12c2151a3436b77d06cce9ef854acd32dd82c1d1
harshvardhan-dot <vardhanharsh012345@gmail.com>
2026-06-08 11:56:32 +0530

    removed an error

--- day10/Q40_characterpyramid.cpp
+++ day10/Q40_characterpyramid.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void pattern(int n)
 {
 
-    for (int i = 0; i <= n; i++) // rows
+    for (int i = 0; i < n; i++) // rows
     {
 
         for (int j = 0; j <= n - i -1; j++) // spaces
